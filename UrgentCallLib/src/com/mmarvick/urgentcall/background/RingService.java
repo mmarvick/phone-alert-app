a96d4f576f02b38ee66fbb078b6f2e44c3b6131d
Michael Marvick <mmarvick@gmail.com>
2014-07-01 00:06:05 -0700

    Updated RingService to handle the call volumes, tones, and ring/vibrate styles for the repeated call and single call alerts

--- UrgentCallLib/src/com/mmarvick/urgentcall/background/RingService.java
+++ UrgentCallLib/src/com/mmarvick/urgentcall/background/RingService.java
@@ -2,6 +2,10 @@ package com.mmarvick.urgentcall.background;
 
 import java.io.IOException;
 
+import com.mmarvick.urgentcall.Constants;
+import com.mmarvick.urgentcall.data.PrefHelper;
+import com.mmarvick.urgentcall.data.RulesDbContract.RulesEntry;
+
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -11,11 +15,18 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.IBinder;
+import android.os.Vibrator;
 
 public class RingService extends Service {
 	private AudioManager audio;
 	private MediaPlayer media;
+	private Vibrator vibrator;
 	private int streamVolumeInit;
+	
+	private float volume;
+	private Uri toneUri;
+	private boolean ring;
+	private boolean vibrate;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -24,31 +35,91 @@ public class RingService extends Service {
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startid) {
-		audio = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
-		media = new MediaPlayer();
-		Uri toneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+		int alertType = intent.getIntExtra(Constants.ALERT_TYPE, Constants.CALL_ALERT_TYPE_BOTH);
 		
-		streamVolumeInit = audio.getStreamVolume(AudioManager.STREAM_ALARM);
-		audio.setStreamVolume(AudioManager.STREAM_ALARM, audio.getStreamMaxVolume(AudioManager.STREAM_ALARM), 0);
-
-		try {
-			media.setDataSource(this, toneUri);
-			media.setAudioStreamType(AudioManager.STREAM_ALARM);
-			media.setLooping(true);
-			media.prepare();
-			media.start();
-		} catch (Exception e) {
-			// TODO Can't play media
+		ring = false;
+		vibrate = false;
+		
+		float rcVolume = PrefHelper.getMessageVolumeValue(getApplicationContext(), RulesEntry.RC_STATE);
+		float scVolume = PrefHelper.getMessageVolumeValue(getApplicationContext(), RulesEntry.SC_STATE);
+		Uri rcToneUri = PrefHelper.getMessageSound(getApplicationContext(), RulesEntry.RC_STATE);
+		Uri scToneUri = PrefHelper.getMessageSound(getApplicationContext(), RulesEntry.SC_STATE);
+		
+		if (alertType == Constants.CALL_ALERT_TYPE_RC) {
+			volume = rcVolume;
+			setHowToAlert(RulesEntry.RC_STATE);
+			
+		} else if (alertType == Constants.CALL_ALERT_TYPE_SC) {
+			volume = scVolume;
+			setHowToAlert(RulesEntry.SC_STATE);
+		} else {
+			
+			if (rcVolume >= scVolume) {
+				volume = rcVolume;				
+			} else {
+				volume = scVolume;			
+			}
+			
+			setHowToAlert(RulesEntry.SC_STATE);
+			setHowToAlert(RulesEntry.RC_STATE);			
 		}
+
+		if (ring) {
+			audio = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+			
+			audio.setStreamMute(AudioManager.STREAM_RING, true);
+			
+			streamVolumeInit = audio.getStreamVolume(AudioManager.STREAM_ALARM);
+			audio.setStreamVolume(AudioManager.STREAM_ALARM, audio.getStreamMaxVolume(AudioManager.STREAM_ALARM), 0);
+			
+			media = new MediaPlayer();
+			try {
+				media.setVolume(volume, volume);
+				media.setDataSource(this, toneUri);
+				media.setAudioStreamType(AudioManager.STREAM_ALARM);
+				media.setLooping(true);
+				media.prepare();
+				media.start();
+			} catch (Exception e) {
+				// TODO Can't play media
+			}
+		}		
 		
+		if (vibrate) {
+			vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+			long[] pattern = {0, 100, 0};
+			vibrator.vibrate(pattern, 0);		
+		}
 		
 		return Service.START_STICKY;
 	}
 	
 	@Override
 	public void onDestroy() {
-		media.stop();
-		audio.setStreamVolume(AudioManager.STREAM_ALARM, streamVolumeInit, 0);
+		if (ring) {
+			audio.setStreamMute(AudioManager.STREAM_RING, false);
+			audio.setStreamVolume(AudioManager.STREAM_ALARM, streamVolumeInit, 0);
+			media.stop();
+		}
+		
+		if (vibrate) {
+			vibrator.cancel();
+		}
+	}	
+	
+	public void setHowToAlert(String alertType) {
+		String howToAlert = PrefHelper.getMessageHow(getApplicationContext(), alertType);
+		
+		if (howToAlert.equals(Constants.ALERT_HOW_RING) || howToAlert.equals(Constants.ALERT_HOW_RING_AND_VIBE)) {
+			ring = true;
+			toneUri = PrefHelper.getMessageSound(getApplicationContext(), alertType);
+		}
+		
+		if (howToAlert.equals(Constants.ALERT_HOW_VIBE) || howToAlert.equals(Constants.ALERT_HOW_RING_AND_VIBE)) {
+			vibrate = true;
+		}		
 	}
+	
+
 
 }

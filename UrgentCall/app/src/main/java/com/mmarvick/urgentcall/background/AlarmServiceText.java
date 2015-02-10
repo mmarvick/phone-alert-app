bbdea324d1e2c72e6fef0496af2a5ed70b091109
Michael Marvick <mmarvick@gmail.com>
2015-02-10 00:35:17 -0800

    Added dummy notification for receiving message alert

--- UrgentCall/app/src/main/java/com/mmarvick/urgentcall/background/AlarmServiceText.java
+++ UrgentCall/app/src/main/java/com/mmarvick/urgentcall/background/AlarmServiceText.java
@@ -1,10 +1,14 @@
 package com.mmarvick.urgentcall.background;
 
 import com.mmarvick.urgentcall.R;
+import com.mmarvick.urgentcall.activities.TextAlertNotificationActivity;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Handler;
+import android.support.v4.app.NotificationCompat;
 
 public class AlarmServiceText extends AlarmService {
 	private int actualDuration;
@@ -21,6 +25,17 @@ public class AlarmServiceText extends AlarmService {
 
 		actualDuration = 1000 * intent.getIntExtra(DURATION,
                 getResources().getInteger(R.integer.message_alert_time_default));
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, TextAlertNotificationActivity.class), 0);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder.setFullScreenIntent(pendingIntent, true);
+        notificationBuilder.setContentTitle("An urgent message!");
+        notificationBuilder.setContentText("Testing this");
+        notificationBuilder.setSmallIcon(R.drawable.ic_action_call);
+        notificationManager.notify(0, notificationBuilder.build());
 		
 		new Handler().postDelayed(new Runnable() {
 			@Override
